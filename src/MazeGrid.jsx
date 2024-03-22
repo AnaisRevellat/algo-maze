7a3f9e414d018f4d45a700749c57ef94d1b8224e
Hexia <you@example.com>
2024-03-22 19:49:12 +0100

    first commit

---
+++ src/MazeGrid.jsx
@@ -0,0 +1,77 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function MazeGrid() {
+  let initialMaze = [
+    ["wall", "wall", "wall", "wall"],
+    ["start", "path", "path", "wall"],
+    ["wall", "wall", "path", "wall"],
+    ["wall", "wall", "path", "end"],
+    ["wall", "wall", "wall", "wall"],
+  ];
+  const [maze, setMaze] = useState([initialMaze]);
+
+  //first a function to define the height and width
+  function generateMaze(height, width) {
+    let matrix = [];
+    for (let i = 0; i < height; i++) {
+      let row = [];
+      for (let j = 0; j < width; j++) {
+        //let cell = Math.random();
+        row.push("wall");
+      }
+      matrix.push(row);
+    }
+    console.log(matrix);
+
+    const dirs = [
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, 0],
+    ];
+
+    function isCellValid(x, y) {
+      return (
+        (y >= 0 && x >= 0) && (y < height && x < width) && matrix[y][x] === "wall"
+      );
+    }
+
+    function carvePath(x, y) {
+      matrix[y][x] = "path";
+
+      const directions = dirs.sort(() => Math.random() - 0.5);
+
+      for (let [dx, dy] of directions) {
+        const nx = x + dx * 2;
+        const ny = y + dy * 2;
+        if (isCellValid(nx, ny)) {
+          matrix[y + dy][x + dx] = "path";
+          carvePath(nx, ny);
+        }
+      }
+    }
+    carvePath(1, 1);
+    
+    matrix[1][0] = "start";
+    matrix[height - 2][width - 1] = "end";
+    setMaze(matrix);
+  }
+
+  return (
+    <div className="maze-grid">
+      <button className={"maze-button"} onClick={() => generateMaze(10, 10)}>
+        Refresh Maze
+      </button>
+      <div className={"maze"}>
+        {maze.map((row, rowIndex) => (
+          <div className="row">
+            {row.map((cell, cellIndex) => (
+              <div className={`cell ${cell}`}></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
