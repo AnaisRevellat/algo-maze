3b8c5c1f8aa9d2c82c27c24a09547726dab3d84b
Hexia <you@example.com>
2024-03-26 08:33:07 +0100

    carving function

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -9,54 +9,80 @@ export default function MazeGrid() {
     ["wall", "wall", "path", "end"],
     ["wall", "wall", "wall", "wall"],
   ];
+  
   const [maze, setMaze] = useState([initialMaze]);
 
   //first a function to define the height and width
   function generateMaze(height, width) {
     let matrix = [];
+
     for (let i = 0; i < height; i++) {
       let row = [];
       for (let j = 0; j < width; j++) {
-        //let cell = Math.random();
-        row.push("wall");
+        let cell = Math.random();
+          row.push('wall'); 
       }
       matrix.push(row);
     }
     console.log(matrix);
 
-    const dirs = [
-      [0, 1],
-      [1, 0],
-      [0, -1],
-      [-1, 0],
-    ];
-
-    function isCellValid(x, y) {
-      return (
-        (y >= 0 && x >= 0) && (y < height && x < width) && matrix[y][x] === "wall"
-      );
-    }
-
-    function carvePath(x, y) {
-      matrix[y][x] = "path";
-
-      const directions = dirs.sort(() => Math.random() - 0.5);
-
-      for (let [dx, dy] of directions) {
-        const nx = x + dx * 2;
-        const ny = y + dy * 2;
-        if (isCellValid(nx, ny)) {
-          matrix[y + dy][x + dx] = "path";
-          carvePath(nx, ny);
-        }
-      }
-    }
-    carvePath(1, 1);
-    
-    matrix[1][0] = "start";
+    matrix[1][0] = "start"; //matrix[y][x]
     matrix[height - 2][width - 1] = "end";
+
+    //the carving function
+    
+    const dirs = [  //[horizontal direction, vertical direction]
+          [0, 1],
+          [1, 0],
+          [0, -1],
+          [-1, 0],
+        ];
+
+      function carvePath(x,y){
+        
+      }
+
     setMaze(matrix);
   }
+
+
+
+
+
+    
+    
+
+  //   function isCellValid(x, y) {
+  //     if (y >= 0 && x >= 0 && y < height && x < width) {
+  //       console.log(matrix[y][x]); 
+  //       return matrix[x][y] === "wall";
+  //     } else {
+  //       return false; 
+  //     }
+  //   }
+
+  //   function carvePath(x, y) {
+  //    matrix[x][y] = "path";
+
+  //     const directions = dirs.sort(() => Math.random() - 0.5);
+  //     console.log('dirs', directions)
+
+  //     for (let [dx, dy] of directions) {
+  //       const nx = x + dx * 2;
+  //       const ny = y + dy * 2;
+  //       console.log("nx:", nx, "ny:", ny); 
+  //       if (isCellValid(nx, ny)) {
+  //         matrix[y + dy][x + dx] = "path";
+  //         carvePath(nx, ny);
+  //       }
+  //     }
+  //   }
+  //   carvePath(1, 1);
+    
+  //   matrix[1][0] = "start";
+  //   matrix[height - 2][width - 1] = "end";
+  //   setMaze(matrix);
+  // }
 
   return (
     <div className="maze-grid">
